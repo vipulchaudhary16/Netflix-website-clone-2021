a8e5d391a6af7e58f15cc47a3e1ef90beccfdeb1
Vipul R Chaudhary <vipulr6111@gmail.com>
2021-08-16 10:11:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+function toggleHide() {
+    // let btn = document.getElementById('btn');
+    let var1 = document.getElementById('ans-1');
+    if (var1.style.display != 'none') {
+        var1.style.display = 'none';
+    }
+    else {
+        var1.style.display = 'block';
+    }
+}
+function toggleHide2() {
+    // let btn = document.getElementById('btn');
+    let var2 = document.getElementById('ans-2');
+    if (var2.style.display != 'none') {
+        var2.style.display = 'none';
+    }
+    else {
+        var2.style.display = 'block';
+    }
+}
+function toggleHide3() {
+    // let btn = document.getElementById('btn');
+    let var3 = document.getElementById('ans-3');
+    if (var3.style.display != 'none') {
+        var3.style.display = 'none';
+    }
+    else {
+        var3.style.display = 'block';
+    }
+}
+function toggleHide4() {
+    // let btn = document.getElementById('btn');
+    let var4 = document.getElementById('ans-4');
+    if (var4.style.display != 'none') {
+        var4.style.display = 'none';
+    }
+    else {
+        var4.style.display = 'block';
+    }
+}
+function toggleHide5() {
+    // let btn = document.getElementById('btn');
+    let var5 = document.getElementById('ans-5');
+    if (var5.style.display != 'none') {
+        var5.style.display = 'none';
+    }
+    else {
+        var5.style.display = 'block';
+    }
+}
+function toggleHide6() {
+    // let btn = document.getElementById('btn');
+    let var6 = document.getElementById('ans-6');
+    if (var6.style.display != 'none') {
+        var6.style.display = 'none';
+    }
+    else {
+        var6.style.display = 'block';
+    }
+}
